5b522b1fb3514ac2f323fbd198d90f65829db121
ab <a.basukoski@gmail.com>
2012-12-04 13:09:16 +0000

    Added create for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,13 @@ class UsersController < ApplicationController
     puts "in UsersController:new"
     @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #Handle success
+    else
+      render 'new'
+    end
+  end
 end
