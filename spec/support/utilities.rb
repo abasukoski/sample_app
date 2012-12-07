a17ebc8409a8fa21e3af6e7ffb4c97e1302dc6a3
ab <a.basukoski@gmail.com>
2012-12-07 19:38:51 +0000

    Refactoring exercieses 8 complete

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,7 +1,7 @@
 include ApplicationHelper
 
 def valid_signin(user)
-  fill_in "Email",    with: user.email
+  fill_in "Email", with: user.email
   fill_in "Password", with: user.password
   click_button "Sign in"
 end
@@ -12,9 +12,15 @@ RSpec::Matchers.define :have_error_message do |message|
   end
 end
 
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success', text: message)
+  end
+end
+
 def sign_in(user)
   visit signin_path
-  fill_in "Email",    with: user.email
+  fill_in "Email", with: user.email
   fill_in "Password", with: user.password
   click_button "Sign in"
   # Sign in when not using Capybara as well.
