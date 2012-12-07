becaeed0286302442bc5c108fddd48a071abf4cd
ab <a.basukoski@gmail.com>
2012-12-07 19:02:06 +0000

    Tests pass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def edit
+    @user = User.find (params[:id])
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -16,6 +20,17 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 end
