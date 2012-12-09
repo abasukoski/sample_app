2cc23f9a03015388982e928e663669b91bfdfa57
ab <a.basukoski@gmail.com>
2012-12-09 19:56:35 +0000

    XXX - error state

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
 
   before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user, only:   [:edit, :update]
 
   def edit
     @user = User.find (params[:id])
   end
 
   def show
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
   end
 
   def new
@@ -26,7 +27,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
@@ -38,7 +39,11 @@ class UsersController < ApplicationController
 
   private
     def signed_in_user
-      #binding.pry
       redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
 end
